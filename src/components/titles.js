38c6ba810fa4a002851dbe2e0884970299abe3f4
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-21 18:07:47 -0500

    Update

---
+++ src/components/titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titles = () => {
+    return (
+        <div>
+            <h1>Weather Info</h1>
+            <p>Search for a city to see the current weather conditions.</p>
+        </div>
+    )
+}
+export default Titles;
