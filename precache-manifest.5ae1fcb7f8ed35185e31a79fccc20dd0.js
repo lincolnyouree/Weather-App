208ad4158db89fb98d5fbbc2df347740a3b4a917
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-21 18:09:46 -0500

    Updates

---
+++ precache-manifest.5ae1fcb7f8ed35185e31a79fccc20dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525589208e7ff20114a0d634c123621d",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "f75242f7c2c258af4817",
+    "url": "/Weather-App/static/css/main.13d1d768.chunk.css"
+  },
+  {
+    "revision": "1f0eb9fd9db152d7eacf",
+    "url": "/Weather-App/static/js/2.85d9e18c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.85d9e18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75242f7c2c258af4817",
+    "url": "/Weather-App/static/js/main.5f03032a.chunk.js"
+  },
+  {
+    "revision": "433bca1ca56c2801eabc",
+    "url": "/Weather-App/static/js/runtime-main.fbdf0f19.js"
+  }
+]);
